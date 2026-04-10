caf75c26860da1eb7b91609d349db251588acc9f
Michał Isalski <isalski.michal@gmail.com>
2026-04-10 23:08:17 +0200

    Enabled framebuffer writethrough for faster drawing

--- kernel/src/memory/mod.rs
+++ kernel/src/memory/mod.rs
@@ -3,15 +3,24 @@ mod frame_allocator;
 mod heap;
 mod memory_init;
 mod page_table;
+
 use alloc::sync::Arc;
-use bootloader_api::BootInfo;
-use internal_utils::kernel_information::frame_allocator::{FullFrameAllocator, print_memory};
+use bootloader_api::{BootInfo, info::FrameBuffer};
+use internal_utils::{
+    kernel_information::frame_allocator::{FullFrameAllocator, print_memory},
+    logln,
+};
 use memory_init::init_page_tables;
 
 use spin::{Mutex, Once};
 use x86_64::{
+    VirtAddr,
+    instructions::tlb,
     registers::control::{Cr3, Cr3Flags},
-    structures::paging::PhysFrame,
+    structures::paging::{
+        Mapper, Page, PageTableFlags, PhysFrame, Translate, mapper::TranslateResult,
+        page::PageRange,
+    },
 };
 
 use crate::memory::{
@@ -30,6 +39,10 @@ pub fn init_kernel_memory(
 
     let mut allocator = BitmapFrameAllocator::init(boot_info);
     init_page_tables(boot_info);
+
+    if let Some(frame_buffer) = boot_info.framebuffer.as_ref() {
+        enable_framebuffer_writethrough(frame_buffer);
+    }
 
     let mut mapper = MEMORY_MAPPER.lock();
     init_heap(mapper.as_mut().unwrap(), &mut allocator).expect("heap initialization failed");
@@ -60,4 +73,41 @@ pub fn with_kernel_memory<V>(action: impl FnOnce() -> V) -> V {
         Cr3::write(cr3.0, cr3.1);
     }
     result
+}
+
+fn enable_framebuffer_writethrough(frame_buffer: &FrameBuffer) {
+    let buffer = VirtAddr::new(frame_buffer.buffer().as_ptr().addr() as u64);
+    let buffer_len = frame_buffer.info().byte_len as u64;
+
+    let mut memlock = MEMORY_MAPPER.lock();
+    let mapper = memlock.as_mut().unwrap();
+
+    let range: PageRange = Page::range(
+        Page::containing_address(buffer),
+        Page::containing_address(buffer + buffer_len),
+    );
+
+    for page in range {
+        let translation = mapper.translate(page.start_address());
+        match translation {
+            TranslateResult::Mapped {
+                frame: _,
+                offset: _,
+                flags,
+            } => {
+                unsafe {
+                    mapper
+                        .update_flags(
+                            page,
+                            flags | PageTableFlags::NO_CACHE | PageTableFlags::WRITE_THROUGH,
+                        )
+                        .unwrap()
+                        .ignore();
+                };
+            }
+            _ => panic!("Something is really wrong with the framebuffer's mapping"),
+        }
+    }
+    tlb::flush_all();
+    logln!("Enabled framebuffer write-through");
 }
